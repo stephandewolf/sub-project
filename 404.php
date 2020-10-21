3bdc179db9ece213a591ed862c55eed94e33fd6f
stephandewolf <stephan@maerke.nl>
2020-10-21 20:21:16 +0200

    201021 – v.1.0.0 – first push towards sub-project (should also sync in root-test).

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Template Name: Page – 404
+ *
+ * @package Maerke – theme template 
+ */
+	
+get_header();
+?>
+
+<main>
+	
+	<?php get_template_part( 'template-parts/blocks/404' ); ?>
+	
+</main>
+
+<?php get_footer(); ?>
